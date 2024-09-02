ab12e88a0d4687f128724b788faa7413eb24310f
Owen Graham <owen.graham@outlook.com>
2024-09-02 13:57:35 +0100

    added unit test directory with one dummy test, testing CI pipeline

---
+++ framework/src/test/java/com/sparta/selenium_project/junit/LoginUnitTest.java
@@ -0,0 +1,11 @@
+package com.sparta.selenium_project.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoginUnitTest {
+    @Test
+    public void test(){
+        Assertions.assertTrue(true);
+    }
+}

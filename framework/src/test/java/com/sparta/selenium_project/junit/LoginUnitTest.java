51788f934650890e1dc9f363b688e90fbfd6dcee
Owen Graham <owen.graham@outlook.com>
2024-09-23 11:14:25 +0100

    a few unit tests for login page written, doing a pull request to check CI pipeine

--- framework/src/test/java/com/sparta/selenium_project/junit/LoginUnitTest.java
+++ framework/src/test/java/com/sparta/selenium_project/junit/LoginUnitTest.java
@@ -1,11 +1,53 @@
 package com.sparta.selenium_project.junit;
 
+import com.sparta.selenium_project.pages.LoginPage;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openqa.selenium.WebElement;
+
+
+import java.lang.reflect.Field;
+
+import static org.mockito.Mockito.verify;
 
 public class LoginUnitTest {
+    @Mock
+    private WebElement usernameField;
+
+    @Mock
+    private WebElement passwordField;
+
+    @InjectMocks
+    private LoginPage loginPage;
+
+    @BeforeEach
+    public void setUp() throws NoSuchFieldException, IllegalAccessException {
+        MockitoAnnotations.openMocks(this);
+        // Using reflection to inject the mock WebElement into the LoginPage
+        Field usernameFieldInPage = LoginPage.class.getDeclaredField("usernameField");
+        usernameFieldInPage.setAccessible(true); // Allow access to the private field
+        usernameFieldInPage.set(loginPage, usernameField); // Inject the mock WebElement
+
+        Field passwordFieldInPage = LoginPage.class.getDeclaredField("passwordField");
+        passwordFieldInPage.setAccessible(true);
+        passwordFieldInPage.set(loginPage,passwordField);
+    }
+
     @Test
-    public void test(){
-        Assertions.assertTrue(true);
+    public void enterUsernameTest(){
+        String username = "username";
+        loginPage.enterUsername(username);
+        verify(usernameField).sendKeys(username);
+    }
+
+    @Test
+    public void enterPasswordTest(){
+        String password = "password";
+        loginPage.enterPassword(password);
+        verify(passwordField).sendKeys(password);
     }
 }
